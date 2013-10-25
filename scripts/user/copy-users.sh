c0862421133be3a73c37fba8211dee1190a620a5
Chris Kuehl <chris@techxonline.net>
2013-10-25 12:48:50 -0700

    Have copy-users copy to storage.

--- scripts/user/copy-users.sh
+++ scripts/user/copy-users.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for HOST in "sandbox" "apache" "client-shell"; do
+for HOST in "sandbox" "apache" "client-shell" "storage"; do
 	echo "Copying to $HOST..."
 	scp -r /srv/users/* "root@$HOST":/srv/users/
 	scp /srv/scripts/srv-update-users.sh "root@$HOST":/srv/scripts/
